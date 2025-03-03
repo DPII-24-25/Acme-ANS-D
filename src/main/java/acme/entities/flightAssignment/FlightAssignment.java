0eeda352f5c19436ac021f9dd6efc3cc6ac380fe
mohanad abulatifa <mohanad.abulatifa@gmail.com>
2025-03-04 00:10:04 +0100

    Flight assignment done

---
+++ src/main/java/acme/entities/flightAssignment/FlightAssignment.java
@@ -0,0 +1,56 @@
+
+package acme.entities.flightAssignment;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import acme.client.components.basis.AbstractEntity;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.Optional;
+import acme.client.components.validation.ValidMoment;
+import acme.client.components.validation.ValidString;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class FlightAssignment extends AbstractEntity {
+
+	private static final long		serialVersionUID	= 1L;
+
+	@Enumerated(EnumType.STRING)
+	@Mandatory
+	private FlightCrewDuty			duty;
+
+	@ValidMoment(past = true)
+	@Mandatory
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date					lastUpdate;
+
+	@Enumerated(EnumType.STRING)
+	@Mandatory
+	private FlightAssignmentStatus	status;
+
+	@ValidString(max = 255)
+	@Optional
+	private String					remarks;
+
+	/*
+	 * @ManyToOne(optional = false)
+	 * 
+	 * @Valid
+	 * private FlightCrewMember flightCrewMember;
+	 * 
+	 * @ManyToOne(optional = false)
+	 * 
+	 * @Valid
+	 * private FlightLeg flightLeg;
+	 * 
+	 */
+}
