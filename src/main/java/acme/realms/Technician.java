fa535f143d6c0acb1ff9d1cf81a45170d5b14b08
JCLPV <80278014+JCLPV@users.noreply.github.com>
2025-03-03 15:37:33 +0100

    Create Technician.java

---
+++ src/main/java/acme/realms/Technician.java
@@ -0,0 +1,63 @@
+
+package acme.realms;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.validation.Valid;
+
+import acme.client.components.basis.AbstractRole;
+import acme.client.components.mappings.Automapped;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.Optional;
+import acme.client.components.validation.ValidNumber;
+import acme.client.components.validation.ValidString;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Technician extends AbstractRole {
+
+	// Serialisation version --------------------------------------------------
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@Mandatory
+	@Column(unique = true)
+	@ValidString(pattern = "^[A-Z]{2-3}\\d{6}$")
+	@Automapped
+	private String				licenseNum;
+
+	@Mandatory
+	@ValidString(pattern = "^\\+?\\d{6,15}$")
+	@Automapped
+	private String				contactPhoneNum;
+
+	@Mandatory
+	@ValidString(max = 50)
+	@Automapped
+	private String				specialisation;
+
+	@Mandatory
+	@Valid
+	@Automapped
+	private Boolean				annoHealthTest;
+
+	@Mandatory
+	@ValidNumber(min = 0)
+	@Automapped
+	private Integer				yearsOfExp;
+
+	@Optional
+	@ValidString(max = 50)
+	@Automapped
+	private String				certification;
+
+	// Derived attributes -----------------------------------------------------
+
+	// Relationships ----------------------------------------------------------
+
+}
