2a287d899f7fca0937424b44a8fd0fcd3c3dcba9
Ronald752 <ronmonalb@alum.us.es>
2025-03-08 20:54:42 +0100

    feat: Add claim entity

---
+++ src/main/java/acme/entities/Claim.java
@@ -0,0 +1,60 @@
+
+package acme.entities;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.ManyToOne;
+import javax.validation.Valid;
+
+import acme.client.components.basis.AbstractEntity;
+import acme.client.components.mappings.Automapped;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.ValidEmail;
+import acme.client.components.validation.ValidMoment;
+import acme.client.components.validation.ValidString;
+import acme.realms.AssistanceAgent;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Setter
+@Getter
+public class Claim extends AbstractEntity {
+
+	private static final long	serialVersionUID	= 1L;
+
+	@Mandatory
+	@ValidMoment(past = true)
+	@Automapped
+	private Date				registrationMoment;
+
+	@Mandatory
+	@ValidEmail
+	@Automapped
+	private String				passengerEmail;
+
+	@Mandatory
+	@ValidString(max = 255)
+	@Automapped
+	private String				description;
+
+	@Enumerated(EnumType.STRING)
+	@Mandatory
+	private IssuesType			type;
+
+	@Mandatory
+	@Valid
+	@Automapped
+	private Boolean				indicator;
+
+	// Relatrionships -----------------------------------------------------
+
+	@Mandatory
+	@Valid
+	@ManyToOne(optional = false)
+	private AssistanceAgent		assistanceAgent;
+
+}
---
+++ src/main/java/acme/entities/IssuesType.java
@@ -0,0 +1,6 @@
+
+package acme.entities;
+
+public enum IssuesType {
+	FLIGHT_ISSUES, LUGGAGE_ISSUES, SECURITY_INCIDENT, OTHER_ISSUES
+}
