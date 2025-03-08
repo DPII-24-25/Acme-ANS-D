8e6475ae6964424a3f941756c78b85daa665158c
JCLPV <80278014+JCLPV@users.noreply.github.com>
2025-03-08 14:41:39 +0100

    Create Review.java

---
+++ src/main/java/acme/entities/reviews/Review.java
@@ -0,0 +1,70 @@
+
+package acme.entities.reviews;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.Valid;
+
+import acme.client.components.basis.AbstractEntity;
+import acme.client.components.mappings.Automapped;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.Optional;
+import acme.client.components.validation.ValidMoment;
+import acme.client.components.validation.ValidNumber;
+import acme.client.components.validation.ValidString;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+
+public class Review extends AbstractEntity {
+
+	// Serialisation version --------------------------------------------------
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@Mandatory
+	@ValidString(max = 50)
+	@Automapped
+	private String				name;
+
+	@Mandatory
+	@ValidMoment(past = true)
+	@Temporal(TemporalType.TIMESTAMP)
+	@Automapped
+	private Date				deadline;
+
+	@Mandatory
+	@ValidString(max = 50)
+	@Automapped
+	private String				subject;
+
+	@Mandatory
+	@ValidString(max = 255)
+	@Automapped
+	private String				text;
+
+	@Optional
+	@ValidNumber(min = 0, max = 10, fraction = 1)
+	@Automapped
+	private Double				score;
+
+	@Optional
+	@Valid
+	@Automapped
+	private Boolean				recommended;
+
+	// Derived attributes -----------------------------------------------------
+
+	// Relationships ----------------------------------------------------------
+
+	//Tiene una relation con Airport
+
+}
