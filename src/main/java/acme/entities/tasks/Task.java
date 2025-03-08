90b8364da4ebaa0e7e2562cb271c6d283711abd8
JCLPV <80278014+JCLPV@users.noreply.github.com>
2025-03-08 14:56:38 +0100

    created Task entity and type enum

---
+++ src/main/java/acme/entities/tasks/Task.java
@@ -0,0 +1,61 @@
+
+package acme.entities.tasks;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.Valid;
+
+import acme.client.components.basis.AbstractEntity;
+import acme.client.components.mappings.Automapped;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.ValidMoment;
+import acme.client.components.validation.ValidNumber;
+import acme.client.components.validation.ValidString;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+
+public class Task extends AbstractEntity {
+
+	// Serialisation version --------------------------------------------------
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@Mandatory
+	@Valid
+	@Automapped
+	private Type				type;
+
+	@Mandatory
+	@ValidString(max = 255)
+	@Automapped
+	private String				description;
+
+	@Mandatory
+	@ValidMoment
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date				deadline;
+
+	@Mandatory
+	@ValidNumber(min = 0, max = 10)
+	@Automapped
+	private Integer				priority;
+
+	@Mandatory
+	@ValidNumber(min = 0)
+	@Automapped
+	private Integer				estimatedDuration;
+
+	// Derived attributes -----------------------------------------------------
+
+	// Relationships ----------------------------------------------------------
+	//Tiene relationships con Technician, Aircraft, y Maintenance Record
+}
---
+++ src/main/java/acme/entities/tasks/Type.java
@@ -0,0 +1,6 @@
+
+package acme.entities.tasks;
+
+public enum Type {
+	MANITENANCE, INSPECTION, REPAIR, SYSTEM_CHECK
+}
