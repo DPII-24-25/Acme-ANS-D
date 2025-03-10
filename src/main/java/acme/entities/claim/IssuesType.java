355b5eb1047232a2964138750a062b22a4e2e30d
Ronald752 <ronmonalb@alum.us.es>
2025-03-10 22:49:14 +0100

    feat: Add validator for service entity

--- src/main/java/acme/entities/claim/IssuesType.java
+++ src/main/java/acme/entities/claim/IssuesType.java
@@ -1,5 +1,5 @@
 
-package acme.entities;
+package acme.entities.claim;
 
 public enum IssuesType {
 	FLIGHT_ISSUES, LUGGAGE_ISSUES, SECURITY_INCIDENT, OTHER_ISSUES
--- src/main/java/acme/entities/service/Service.java
+++ src/main/java/acme/entities/service/Service.java
@@ -1,7 +1,8 @@
 
-package acme.entities;
+package acme.entities.service;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 
 import acme.client.components.basis.AbstractEntity;
 import acme.client.components.datatypes.Money;
@@ -12,7 +13,14 @@ import acme.client.components.validation.ValidMoney;
 import acme.client.components.validation.ValidNumber;
 import acme.client.components.validation.ValidString;
 import acme.client.components.validation.ValidUrl;
+import acme.constraints.ValidService;
+import lombok.Getter;
+import lombok.Setter;
 
+@Entity
+@Getter
+@Setter
+@ValidService
 public class Service extends AbstractEntity {
 
 	private static final long	serialVersionUID	= 1L;
