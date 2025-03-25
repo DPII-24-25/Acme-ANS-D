eab6c0e474fc1fc37e8482ad29d8ba93d4578af9
Ronald752 <ronmonalb@alum.us.es>
2025-03-26 00:00:10 +0100

    feat: Add list tracking-log by claimId

---
+++ src/main/java/acme/features/assistance_agent/tracking_log/AssistanceAgentTrackingLogController.java
@@ -0,0 +1,25 @@
+
+package acme.features.assistance_agent.tracking_log;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import acme.client.controllers.AbstractGuiController;
+import acme.client.controllers.GuiController;
+import acme.entities.trackinglog.TrackingLog;
+import acme.realms.AssistanceAgent;
+
+@GuiController
+public class AssistanceAgentTrackingLogController extends AbstractGuiController<AssistanceAgent, TrackingLog> {
+
+	@Autowired
+	private AssistanceAgentTrackingLogListService listService;
+
+
+	@PostConstruct
+	protected void initialise() {
+		super.addBasicCommand("list", this.listService);
+	}
+
+}
---
+++ src/main/java/acme/features/assistance_agent/tracking_log/AssistanceAgentTrackingLogRepository.java
@@ -0,0 +1,22 @@
+
+package acme.features.assistance_agent.tracking_log;
+
+import java.util.Collection;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import acme.client.repositories.AbstractRepository;
+import acme.entities.claim.Claim;
+import acme.entities.trackinglog.TrackingLog;
+
+@Repository
+public interface AssistanceAgentTrackingLogRepository extends AbstractRepository {
+
+	@Query("select c from Claim c where c.id =:id")
+	Claim findClaimById(int id);
+
+	@Query("select t from TrackingLog t where t.claim.id =:id")
+	Collection<TrackingLog> findTrackingLogsByClaimId(int id);
+
+}
