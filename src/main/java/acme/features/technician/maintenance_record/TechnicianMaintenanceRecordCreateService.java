408ef7d3cb320c1dcc865f972329114227f436d0
JCLPV <80278014+JCLPV@users.noreply.github.com>
2025-04-01 17:57:40 +0200

    fixes MR services plus created Delete service

---
+++ src/main/java/acme/features/technician/maintenance_record/TechnicianMaintenanceRecordCreateService.java
@@ -0,0 +1,95 @@
+/*
+ * TechnicianMaintenanceRecordCreateService.java
+ *
+ * Copyright (C) 2012-2025 Rafael Corchuelo.
+ *
+ * In keeping with the traditional purpose of furthering education and research, it is
+ * the policy of the copyright owner to permit non-commercial use and redistribution of
+ * this software. It has been tested carefully, but it is not guaranteed for any particular
+ * purposes. The copyright owner does not offer any warranties or representations, nor do
+ * they accept any liabilities with respect to them.
+ */
+
+package acme.features.technician.maintenance_record;
+
+import java.util.Collection;
+
+import acme.client.components.models.Dataset;
+import acme.client.components.views.SelectChoices;
+import acme.client.services.AbstractGuiService;
+import acme.client.services.GuiService;
+import acme.entities.aircraft.Aircraft;
+import acme.entities.maintenance_records.MaintenanceRecord;
+import acme.realms.Technician;
+
+@GuiService
+public class TechnicianMaintenanceRecordCreateService extends AbstractGuiService<Technician, MaintenanceRecord> {
+
+	// Internal state ---------------------------------------------------------
+
+	//@Autowired
+	private TechnicianMaintenanceRecordRepository repository;
+
+	// AbstractGuiService interface -------------------------------------------
+
+
+	@Override
+	public void authorise() {
+		super.getResponse().setAuthorised(true);
+	}
+
+	@Override
+	public void load() {
+		MaintenanceRecord record1;
+		Technician technician;
+
+		technician = (Technician) super.getRequest().getPrincipal().getActiveRealm();
+
+		record1 = new MaintenanceRecord();
+		record1.setDraftMode(true);
+		record1.setTechnician(technician);
+
+		super.getBuffer().addData(record1);
+	}
+
+	@Override
+	public void bind(final MaintenanceRecord record1) {
+		int aircraftId;
+		Aircraft aircraft;
+
+		aircraftId = super.getRequest().getData("contractor", int.class);
+		aircraft = this.repository.findAircraftById(aircraftId);
+
+		super.bindObject(record1, "moment", "status", "inspectDueDate", "estCost", "moreInfo");
+		record1.setAircraft(aircraft);
+	}
+
+	@Override
+	public void validate(final MaintenanceRecord record1) {
+		;
+	}
+
+	@Override
+	public void perform(final MaintenanceRecord record1) {
+		this.repository.save(record1);
+	}
+
+	@Override
+	public void unbind(final MaintenanceRecord record1) {
+		//int technicianId;
+		Collection<Aircraft> aircrafts;
+		SelectChoices choices;
+		Dataset dataset;
+
+		//technicianId = super.getRequest().getPrincipal().getActiveRealm().getId();
+		aircrafts = this.repository.findAllAircrafts();
+		choices = SelectChoices.from(aircrafts, "aircraft", record1.getAircraft());
+
+		dataset = super.unbindObject(record1, "moment", "status", "inspectDueDate", "estCost", "moreInfo", "draftMode");
+		dataset.put("aircraft", choices.getSelected().getKey());
+		dataset.put("aircrafts", choices);
+
+		super.getResponse().addData(dataset);
+	}
+
+}
---
+++ src/main/java/acme/features/technician/maintenance_record/TechnicianMaintenanceRecordListService.java
@@ -0,0 +1,59 @@
+/*
+ * TechnicianMaintenanceRecordListService.java
+ *
+ * Copyright (C) 2012-2025 Rafael Corchuelo.
+ *
+ * In keeping with the traditional purpose of furthering education and research, it is
+ * the policy of the copyright owner to permit non-commercial use and redistribution of
+ * this software. It has been tested carefully, but it is not guaranteed for any particular
+ * purposes. The copyright owner does not offer any warranties or representations, nor do
+ * they accept any liabilities with respect to them.
+ */
+
+package acme.features.technician.maintenance_record;
+
+import java.util.Collection;
+
+import acme.client.components.models.Dataset;
+import acme.client.services.AbstractGuiService;
+import acme.client.services.GuiService;
+import acme.entities.maintenance_records.MaintenanceRecord;
+import acme.realms.Technician;
+
+@GuiService
+public class TechnicianMaintenanceRecordListService extends AbstractGuiService<Technician, MaintenanceRecord> {
+
+	// Internal state ---------------------------------------------------------
+
+	//@Autowired
+	private TechnicianMaintenanceRecordRepository repository;
+
+	// AbstractGuiService interface -------------------------------------------
+
+
+	@Override
+	public void authorise() {
+		super.getResponse().setAuthorised(true);
+	}
+
+	@Override
+	public void load() {
+		Collection<MaintenanceRecord> records;
+		int technicianId;
+
+		technicianId = super.getRequest().getPrincipal().getActiveRealm().getId();
+		records = this.repository.findMaintenanceRecordByTechnicianId(technicianId);
+
+		super.getBuffer().addData(records);
+	}
+
+	@Override
+	public void unbind(final MaintenanceRecord record1) {
+		Dataset dataset;
+
+		dataset = super.unbindObject(record1, "status", "aircraft", "inspectDueDate", "draftMode");
+
+		super.getResponse().addData(dataset);
+	}
+
+}
