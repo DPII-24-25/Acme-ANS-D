721bf979ca0f08842600504fecfc333d350243a8
mohanadabulatifa <mohanad.abulatifa@gmail.com>
2025-04-10 19:25:10 +0200

    feat: Add update,create,list and show for activity log

---
+++ src/main/java/acme/features/flight_crew_member/flight_assignment/FlightCrewMemberActivityLogController.java
@@ -0,0 +1,37 @@
+
+package acme.features.flight_crew_member.activity_log;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import acme.client.controllers.AbstractGuiController;
+import acme.client.controllers.GuiController;
+import acme.entities.activityLog.ActivityLog;
+import acme.realms.FlightCrewMember;
+
+@GuiController
+public class FlightCrewMemberActivityLogController extends AbstractGuiController<FlightCrewMember, ActivityLog> {
+
+	@Autowired
+	private FlightCrewMemberActivityLogListService		listService;
+
+	@Autowired
+	private FlightCrewMemberActivityLogShowService		showService;
+
+	@Autowired
+	private FlightCrewMemberActivityLogCreateService	createService;
+
+	@Autowired
+	private FlightCrewMemberActivityLogUpdateService	updateService;
+
+
+	@PostConstruct
+	protected void initialise() {
+		super.addBasicCommand("list", this.listService);
+		super.addBasicCommand("show", this.showService);
+		super.addBasicCommand("create", this.createService);
+		super.addBasicCommand("update", this.updateService);
+	}
+
+}
