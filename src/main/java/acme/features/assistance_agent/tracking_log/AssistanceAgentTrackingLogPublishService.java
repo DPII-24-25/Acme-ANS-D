fddc49fb739f4b8260ea3e716ccad59afbe76a5c
Ronald752 <ronmonalb@alum.us.es>
2025-04-23 17:32:48 +0200

    feat: Add vallidation for update and publish

--- src/main/java/acme/features/assistance_agent/tracking_log/AssistanceAgentTrackingLogPublishService.java
+++ src/main/java/acme/features/assistance_agent/tracking_log/AssistanceAgentTrackingLogPublishService.java
@@ -1,6 +1,8 @@
 
 package acme.features.assistance_agent.tracking_log;
 
+import java.util.Collection;
+import java.util.Comparator;
 import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -76,8 +78,44 @@ public class AssistanceAgentTrackingLogPublishService extends AbstractGuiService
 	}
 
 	@Override
-	public void validate(final TrackingLog object) {
-		assert object != null;
+	public void validate(final TrackingLog trackingLog) {
+		assert trackingLog != null;
+
+		/*
+		 * VALIDAR LA OBLIGACION DE LA RESOLUCION SI EL ESTADO ES DIFERENTE DE PENDING
+		 */
+
+		if (trackingLog.getStatus() != null) {
+			boolean condicion = trackingLog.getStatus() != TypeStatus.PENDING && trackingLog.getResolution().isEmpty();
+			super.state(!condicion, "resolution", "Es obligatorio si es status es ACCEPTED o REJECTED");
+		}
+
+		// COMPROBAR SI ES  NECESARIO VERIFICAR ESTO, EN CASO SEA NECESARIO, SE PODRIA OBLLIGAR A QUE SEA MAYOR AL ULTIMO PUBLICADO
+
+		if (trackingLog.getResolutionPorcentage() != null) {
+			Collection<TrackingLog> lsTL = this.repository.findTrackingLogsByClaimId(trackingLog.getClaim().getId());
+			Double maxPorcentage = lsTL.isEmpty() ? 0 : lsTL.stream().max(Comparator.comparing(TrackingLog::getResolutionPorcentage)).get().getResolutionPorcentage();
+			boolean preCondicion = trackingLog.getResolutionPorcentage() == 100 && maxPorcentage == 100;
+			boolean condicion = trackingLog.getResolutionPorcentage() > maxPorcentage || preCondicion;
+			super.state(condicion, "resolutionPorcentage", "The resolution porcentage shoud be higher than the maximum resolution porcentage of the claim");
+
+			// COMPRUEBO SI EXISTEN TL CON MENOR VALOR DE 100 SIN PUBLICAR, DE ESA MANERA ASEGURO QUE EL DE 100 SEA EL ULTIMO EN PUBLICAR
+			boolean preCondicion2 = trackingLog.getResolutionPorcentage() == 100;
+			boolean condicion2 = lsTL.stream().filter(x -> x.isDraftMode() && x.getResolutionPorcentage() < 100).count() > 0 && preCondicion2;
+			super.state(!condicion2, "resolutionPorcentage", "No se puede publicar un tracking log con porcentage 100 porque existen otros sin publicar de menor valor.");
+		}
+
+		/*
+		 * VALIDAR QUE UN TR CON PORCENTAGE 100 DEBA TENER ESTADO DIFERENTE DE PENDING Y VISCEVERSA
+		 */
+
+		if (trackingLog.getStatus() != null && trackingLog.getResolutionPorcentage() != null) {
+			boolean condicion = trackingLog.getStatus() != TypeStatus.PENDING && trackingLog.getResolutionPorcentage() != 100;
+			super.state(!condicion, "resolutionPorcentage", "The resolution porcentage should be one hundred percent");
+
+			boolean condicion2 = trackingLog.getStatus() == TypeStatus.PENDING && trackingLog.getResolutionPorcentage() == 100;
+			super.state(!condicion2, "status", "The status should be ACCEPTED or REJECTED when resolutionPorcentage is one hundred");
+		}
 	}
 
 	@Override
