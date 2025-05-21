63194cb694b951f0a01649ce444d31887f5b2195
Ronald752 <ronmonalb@alum.us.es>
2025-05-21 13:21:02 +0200

    feat: Add validator for trackinglog

--- src/main/java/acme/constraints/TrackingLogValidator.java
+++ src/main/java/acme/constraints/TrackingLogValidator.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import acme.client.components.validation.AbstractValidator;
 import acme.entities.trackinglog.TrackingLog;
 import acme.entities.trackinglog.TrackingLogRepository;
+import acme.entities.trackinglog.TypeStatus;
 
 public class TrackingLogValidator extends AbstractValidator<ValidTrackingLog, TrackingLog> {
 
@@ -26,6 +27,14 @@ public class TrackingLogValidator extends AbstractValidator<ValidTrackingLog, Tr
 
 		if (trackingLog == null)
 			super.state(context, false, "*", "javax.validation.constraints.NotNull.message");
+		else if (trackingLog.getStatus().equals(TypeStatus.PENDING) && trackingLog.getResolutionPorcentage() == 100.00)
+			super.state(context, false, "Status", "The status can be “PENDING” only when the resolution percentage is not 100%");
+		else if (!trackingLog.getStatus().equals(TypeStatus.PENDING) && trackingLog.getResolutionPorcentage() != 100.00)
+			super.state(context, false, "Status", "The status can be “ACCEPTED” or “REJECTED” only when the resolution percentage gets to 100%");
+		else if (!trackingLog.getStatus().equals(TypeStatus.PENDING) && (trackingLog.getResolution() == null || trackingLog.getResolution().isBlank()))
+			super.state(context, false, "Status", "If the status is not “PENDING”, then the resolution is mandatory");
+		else if (trackingLog.getClaim().isDraftMode())
+			super.state(context, false, "Claim", "We cannot associate a tracking log with a claim in draft mode.");
 
 		return !super.hasErrors(context);
 	}
