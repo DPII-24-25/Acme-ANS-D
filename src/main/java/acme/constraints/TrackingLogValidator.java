09fab44b94d5262e7379c25cd2d63ba3f9e47141
Ronald752 <ronmonalb@alum.us.es>
2025-05-22 18:08:33 +0200

    feat: Modify validator

--- src/main/java/acme/constraints/TrackingLogValidator.java
+++ src/main/java/acme/constraints/TrackingLogValidator.java
@@ -24,18 +24,22 @@ public class TrackingLogValidator extends AbstractValidator<ValidTrackingLog, Tr
 	@Override
 	public boolean isValid(final TrackingLog trackingLog, final ConstraintValidatorContext context) {
 		assert context != null;
+		boolean isPending = false;
 
 		if (trackingLog == null)
 			super.state(context, false, "*", "javax.validation.constraints.NotNull.message");
-		else if (trackingLog.getStatus().equals(TypeStatus.PENDING) && trackingLog.getResolutionPorcentage() == 100.00)
-			super.state(context, false, "Status", "The status can be “PENDING” only when the resolution percentage is not 100%");
-		else if (!trackingLog.getStatus().equals(TypeStatus.PENDING) && trackingLog.getResolutionPorcentage() != 100.00)
-			super.state(context, false, "Status", "The status can be “ACCEPTED” or “REJECTED” only when the resolution percentage gets to 100%");
-		else if (!trackingLog.getStatus().equals(TypeStatus.PENDING) && (trackingLog.getResolution() == null || trackingLog.getResolution().isBlank()))
-			super.state(context, false, "Status", "If the status is not “PENDING”, then the resolution is mandatory");
-		else if (trackingLog.getClaim().isDraftMode())
+		else if (trackingLog.getStatus() != null && trackingLog.getResolutionPorcentage() != null) {
+			isPending = trackingLog.getStatus().equals(TypeStatus.PENDING);
+			if (isPending && trackingLog.getResolutionPorcentage() == 100.00)
+				super.state(context, false, "status", "The status can be “PENDING” only when the resolution percentage is not 100%");
+			else if (!isPending && trackingLog.getResolutionPorcentage() != 100.00)
+				super.state(context, false, "status", "The status can be “ACCEPTED” or “REJECTED” only when the resolution percentage gets to 100%");
+		} else if (trackingLog.getStatus() != null && trackingLog.getResolution() != null) {
+			isPending = trackingLog.getStatus().equals(TypeStatus.PENDING);
+			if (!isPending && (trackingLog.getResolution() == null || trackingLog.getResolution().isBlank()))
+				super.state(context, false, "status", "If the status is not “PENDING”, then the resolution is mandatory");
+		} else if (trackingLog.getClaim().isDraftMode())
 			super.state(context, false, "Claim", "We cannot associate a tracking log with a claim in draft mode.");
-
 		return !super.hasErrors(context);
 	}
 
